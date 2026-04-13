619a047d08603e5e90fb8c407ecf7d8701683356
漂泊者 <@blackshore-c.com>
2026-04-13 11:21:48 +0800

    ✨ init portfolio site

---
+++ _js/main.js
@@ -0,0 +1,7 @@
+// ================================
+// Main JavaScript
+// ================================
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Site loaded.');
+});
